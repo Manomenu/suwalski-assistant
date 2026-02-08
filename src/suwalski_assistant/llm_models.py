d9cf4b13202e9f608d92979e756de5d173d45e00
Piotr Gugnowski <piotr.gugnowski@adrentech.com>
2026-02-08 20:57:04 +0100

    xd

--- src/suwalski_assistant/llm_models.py
+++ src/suwalski_assistant/llm_models.py
@@ -1,24 +1,24 @@
 import logging
+import litellm
 from google.adk.models.lite_llm import LiteLlm
 from suwalski_assistant.settings import settings
-import litellm
 
 logging.getLogger("LiteLLM").setLevel(logging.CRITICAL)
-logging.info(f"LLM_ENV: {settings.llm_env}")
-
 litellm.telemetry = False
 
-class FixedLiteLlm(LiteLlm):
-    async def acompletion(self, *args, **kwargs):
-        if "messages" in kwargs:
-            for message in kwargs["messages"]:
-                if "content" not in message or message["content"] is None:
-                    message["content"] = ""
-        
-        return await super().acompletion(*args, **kwargs)
+_original_acompletion = litellm.acompletion
 
-ollama_model = FixedLiteLlm(
+async def patched_acompletion(*args, **kwargs):
+    if "messages" in kwargs:
+        for message in kwargs["messages"]:
+            if "content" not in message or message["content"] is None:
+                message["content"] = ""
+    return await _original_acompletion(*args, **kwargs)
+
+litellm.acompletion = patched_acompletion
+
+ollama_model = LiteLlm(
     model=settings.ollama_model, 
     api_key=settings.ollama_api_key, 
-    api_base=settings.ollama_api_base
+    api_base=settings.ollama_api_base.rstrip("/")
 )
